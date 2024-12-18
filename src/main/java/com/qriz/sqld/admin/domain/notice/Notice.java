6139d1e64a5487bbe83f3acd1ac08f92f09a18f3
백승주 <137677440+bearkuang@users.noreply.github.com>
2024-12-18 20:37:32 +0900

    [Update] 특정 공지사항 보기 기능 추가

---
+++ src/main/java/com/qriz/sqld/admin/domain/notice/Notice.java
@@ -0,0 +1,29 @@
+package com.qriz.sqld.admin.domain.notice;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Notice {
+    
+    @Column(name = "notice_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    private LocalDateTime createAt;
+}
